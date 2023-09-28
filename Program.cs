a4ddfec51f8f2ca8e2e3068f6943a9d99084ee5b
Sohail5412 <120371857+Sohail5412@users.noreply.github.com>
2023-09-27 23:52:14 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MyAspNetProject
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    // Build the application.
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
+}
---
+++ site.js
@@ -0,0 +1,48 @@
+﻿// NexaTech Solutions JavaScript
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Initialization message for NexaTech brand
+    console.log('Welcome to NexaTech Solutions');
+
+    // Function to load the main banner image
+    function loadBannerImage() {
+        // Path to the main banner image
+        var bannerImagePath = "C:\\Users\\Sohail Khan\\Documents\\coder\\banner_image.jpg"; // Update the image name
+        var bannerElement = document.getElementById('nexatech-banner');
+        if (bannerElement) {
+            // Set the source URL for the banner image
+            bannerElement.src = bannerImagePath;
+        }
+    }
+
+    // Function to load product images for the NexaTech gallery
+    function loadProductImages() {
+        // Getting the product gallery element
+        var productGallery = document.getElementById('nexatech-product-gallery');
+        if (productGallery) {
+            // Paths for the product images located in the coding directory
+            var imageList = [
+                "C:\\Users\\Sohail Khan\\Documents\\coding\\product_image_1.jpg", // Update image names
+                "C:\\Users\\Sohail Khan\\Documents\\coding\\product_image_2.jpg",
+            ];
+
+            // Loop over the image list and append each image to the product gallery
+            imageList.forEach(function (src) {
+                var img = document.createElement('img');
+                img.src = src;
+                productGallery.appendChild(img);
+            });
+        }
+    }
+
+    // Function to initialize any interactive components for NexaTech
+    function initializeInteractiveComponents() {
+       
+
+    // Invoke the functions to load content 
+    loadBannerImage();
+    loadProductImages();
+    initializeInteractiveComponents();
+
+});
